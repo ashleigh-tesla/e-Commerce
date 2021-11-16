9965a1c0f5f92e2bc0523be02f0d8ea1bcab487e
ashleigh-tesla <84648968+ashleigh-tesla@users.noreply.github.com>
2021-11-16 20:02:34 +0200

    updated index.html & eCommerce.js

--- scripts/eCommerce.js
+++ scripts/eCommerce.js
@@ -42,3 +42,21 @@ document.querySelectorAll('.featured-image3').forEach(image_3 => {
         document.querySelector('.big-image3').src = src
     })
 })
+document.querySelectorAll('.featured-image4').forEach(image_4 => {
+    image_4.addEventListener('mouseover', () => {
+        var src = image_4.getAttribute('src')
+        document.querySelector('.big-image4').src = src
+    })
+})
+document.querySelectorAll('.featured-image5').forEach(image_5 => {
+    image_5.addEventListener('click', () => {
+        var src = image_5.getAttribute('src')
+        document.querySelector('.big-image5').src = src
+    })
+})
+document.querySelectorAll('.featured-image6').forEach(image_6 => {
+    image_6.addEventListener('mouseover', () => {
+        var src = image_6.getAttribute('src')
+        document.querySelector('.big-image6').src = src
+    })
+})
