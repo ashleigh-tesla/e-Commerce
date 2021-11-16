cc1bcd01ed5960a3aaa561fa833c0f4109c8a528
ashleigh-tesla <84648968+ashleigh-tesla@users.noreply.github.com>
2021-11-16 14:41:41 +0200

    added more photos updated index.html updated eCommerce.css updated eCommerce.js

--- scripts/eCommerce.js
+++ scripts/eCommerce.js
@@ -24,3 +24,21 @@ function prev() {
     index = (index - 1 + slides.length) % slides.length
     slides[index].classList.add('active')
 }
+document.querySelectorAll('.featured-image1').forEach(image_1 => {
+    image_1.addEventListener('click', () => {
+        var src = image_1.getAttribute('src')
+        document.querySelector('.big-image1').src = src
+    })
+})
+document.querySelectorAll('.featured-image2').forEach(image_2 => {
+    image_2.addEventListener('mouseover', () => {
+        var src = image_2.getAttribute('src')
+        document.querySelector('.big-image2').src = src
+    })
+})
+document.querySelectorAll('.featured-image3').forEach(image_3 => {
+    image_3.addEventListener('click', () => {
+        var src = image_3.getAttribute('src')
+        document.querySelector('.big-image3').src = src
+    })
+})
